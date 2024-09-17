eea44af1eca01b683b53e82daec65214496a1577
dianafitriaw <dianafit72@gmail.com>
2024-09-17 23:48:23 +0700

    feat: edit style signpage

--- jobhunter/src/app/auth/signIn/page.tsx
+++ jobhunter/src/app/auth/signIn/page.tsx
@@ -22,11 +22,22 @@ export default function SignInPage() {
   };
 
   return (
-    <div className="flex justify-center items-center w-full h-screen">
-      <div className="p-3 shadow-md rounded-md flex flex-col gap-3 justify-center bg-slate-800">
+    <div className="flex justify-center items-center w-full h-screen bg-white">
+      <div className="p-10 shadow-md flex flex-col gap-3 justify-center bg-white">
+        <div className="flex flex-row justify-center items-center gap-2">
+          <img 
+          src="/Vector.png" 
+          alt="icon" 
+          className="w-6 h-6"/>
+          <h1 className="text-steel-blue text-xl font-bold">JobHunter</h1>
+        </div>
+        <div className="flex flex-row justify-center items-center mt-2">
+          <button className="w-24 px-2 py-1 text-steel-blue text-base font-semibold hover:bg-slate-300">Job Seeker</button>
+          <button className="w-24 px-2 py-1 text-steel-blue text-base font-semibold hover:bg-slate-300">Company</button>
+        </div>
         <label className="form-control w-80 ">
           <div className="label">
-            <span className="text-xs tracking-widest">Username?</span>
+            <span className="text-base font-semibold tracking-widest text-dark-grey-text">Username</span>
           </div>
           <input
             type="text"
@@ -34,13 +45,13 @@ export default function SignInPage() {
             onChange={(e) => {
               usernameCurr = e.target.value;
             }}
-            placeholder=". . ."
-            className="input w-80 bg-transparent"
+            placeholder="username"
+            className="input w-80 h-10 border-0 rounded-none bg-white"
           />
         </label>
         <label className="form-control w-80 ">
           <div className="label">
-            <span className="text-xs tracking-widest">Password?</span>
+            <span className="text-base font-semibold tracking-widest text-dark-grey-text">Password</span>
           </div>
           <input
             type="password"
@@ -48,11 +59,11 @@ export default function SignInPage() {
             onChange={(e) => {
               passwordCurr = e.target.value;
             }}
-            placeholder=". . ."
-            className="input w-80 bg-transparent"
+            placeholder="password"
+            className="input w-80 h-10 border-0 rounded-none bg-white"
           />
         </label>
-        <button onClick={onSubmit} className="btn btn-primary">
+        <button onClick={onSubmit} className="btn bg-steel-blue text-white font-semibold text-base border-0 rounded-none hover:bg-slate-400 mt-5">
           Login
         </button>
         <div className="flex justify-center w-full p-5">
