82064fffd8f965da9ff15174e34a0bec7c0ce7a2
dianafitriaw <dianafit72@gmail.com>
2024-09-18 13:40:20 +0700

    feat: edit login page

--- jobhunter/src/app/auth/signIn/page.tsx
+++ jobhunter/src/app/auth/signIn/page.tsx
@@ -23,7 +23,7 @@ export default function SignInPage() {
 
   return (
     <div className="flex justify-center items-center w-full h-screen bg-white">
-      <div className="p-10 shadow-md flex flex-col gap-3 justify-center bg-white">
+      <div className="p-5 shadow-md flex flex-col gap-3 justify-center bg-white">
         <div className="flex flex-row justify-center items-center gap-2">
           <img 
           src="/Vector.png" 
@@ -31,9 +31,9 @@ export default function SignInPage() {
           className="w-6 h-6"/>
           <h1 className="text-steel-blue text-xl font-bold">JobHunter</h1>
         </div>
-        <div className="flex flex-row justify-center items-center mt-2">
-          <button className="w-24 px-2 py-1 text-steel-blue text-base font-semibold hover:bg-slate-300">Job Seeker</button>
-          <button className="w-24 px-2 py-1 text-steel-blue text-base font-semibold hover:bg-slate-300">Company</button>
+        <div className="flex flex-row justify-center items-center">
+          <button className="w-24 px-2 py-2 text-steel-blue text-base font-semibold hover:bg-slate-300">Job Seeker</button>
+          <button className="w-24 px-2 py-2 text-steel-blue text-base font-semibold hover:bg-slate-300">Company</button>
         </div>
         <label className="form-control w-80 ">
           <div className="label">
@@ -46,7 +46,7 @@ export default function SignInPage() {
               usernameCurr = e.target.value;
             }}
             placeholder="username"
-            className="input w-80 h-10 border-0 rounded-none bg-white"
+            className="input text-black input-bordered w-full max-w-xs focus:border-steel-blue rounded-none bg-white"
           />
         </label>
         <label className="form-control w-80 ">
@@ -60,7 +60,7 @@ export default function SignInPage() {
               passwordCurr = e.target.value;
             }}
             placeholder="password"
-            className="input w-80 h-10 border-0 rounded-none bg-white"
+            className="input text-black input-bordered w-full max-w-xs focus:border-steel-blue rounded-none bg-white"
           />
         </label>
         <button onClick={onSubmit} className="btn bg-steel-blue text-white font-semibold text-base border-0 rounded-none hover:bg-slate-400 mt-5">
@@ -71,6 +71,10 @@ export default function SignInPage() {
           redirect:true,
           callbackUrl: '/'
         })} className="mx-auto p-3"/>
+        </div>
+        <div className="flex flex-row justify-center items-center text-base text-dark-grey-text gap-1">
+          <h1>Don't Have an Account?</h1>
+          <a href="/register" className="text-steel-blue"> Register</a>
         </div>
       </div>
     </div>
