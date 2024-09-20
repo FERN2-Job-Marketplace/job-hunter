82235e2dd70b427d0f30d4a0ce02b09d84d45fec
Nicholasnrza <nicholasnrza@gmail.com>
2024-09-20 17:12:50 +0700

    components: CardJobs

---
+++ jobhunter/src/app/_components/CardJobs.tsx
@@ -0,0 +1,122 @@
+export default function CardJobs() {
+    return (
+        <>
+            <div className="w-screen">  
+                <div className="grid grid-cols-4 gap-10 px-32 py-10 items-end">
+                    {/* Card-1 */}
+                    <div className="px-2 py-4 h-[20vw] bg-[#ffffff] hover:bg-[#4682B4] text-black hover:text-[#ffffff] transition-colors">
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-lg font-semibold uppercase">Logo</div>
+                            <button className="px-2.5 py-1 border-2 bg-gray-100 border-[#4682B4] text-sm font-normal text-[#4682B4]">Full Time</button> 
+                        </div> 
+                        <div className="font-bold text-justify text-lg mt-2">UI/UX Designer</div>
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-sm font-bold mt-2">Twitter</div>
+                            <div className="text-sm font-bold mt-2">Jakarta, Indonesia</div>
+                        </div>
+                        <div className="text-sm font-light text-gray-400 my-5">Twitter is looking for UI/UX Designer to help team ma ...</div>
+                        <button className="px-2.5 py-0.5 my-3 bg-gray-100 rounded-full text-base font-medium text-green-500">Design</button> 
+                    </div>
+                    {/* Card-2 */}
+                    <div className="px-2 py-4 h-[20vw] bg-[#ffffff] hover:bg-[#4682B4] text-black hover:text-[#ffffff] transition-colors">
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-lg font-semibold uppercase">Logo</div>
+                            <button className="px-2.5 py-1 border-2 bg-gray-100 border-[#4682B4] text-sm font-normal text-[#4682B4]">Full Time</button> 
+                        </div> 
+                        <div className="font-bold text-justify text-lg mt-2">UI/UX Designer</div>
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-sm font-bold mt-2">Twitter</div>
+                            <div className="text-sm font-bold mt-2">Jakarta, Indonesia</div>
+                        </div>
+                        <div className="text-sm font-light text-gray-400 my-5">Twitter is looking for UI/UX Designer to help team ma ...</div>
+                        <button className="px-2.5 py-0.5 my-3 bg-gray-100 rounded-full text-base font-medium text-green-500">Design</button> 
+                    </div>
+                    {/* Card-3 */}
+                    <div className="px-2 py-4 h-[20vw] bg-[#ffffff] hover:bg-[#4682B4] text-black hover:text-[#ffffff] transition-colors">
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-lg font-semibold uppercase">Logo</div>
+                            <button className="px-2.5 py-1 border-2 bg-gray-100 border-[#4682B4] text-sm font-normal text-[#4682B4]">Full Time</button> 
+                        </div> 
+                        <div className="font-bold text-justify text-lg mt-2">UI/UX Designer</div>
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-sm font-bold mt-2">Twitter</div>
+                            <div className="text-sm font-bold mt-2">Jakarta, Indonesia</div>
+                        </div>
+                        <div className="text-sm font-light text-gray-400 my-5">Twitter is looking for UI/UX Designer to help team ma ...</div>
+                        <button className="px-2.5 py-0.5 my-3 bg-gray-100 rounded-full text-base font-medium text-green-500">Design</button> 
+                    </div>
+                    {/* Card-4 */}
+                    <div className="px-2 py-4 h-[20vw] bg-[#ffffff] hover:bg-[#4682B4] text-black hover:text-[#ffffff] transition-colors">
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-lg font-semibold uppercase">Logo</div>
+                            <button className="px-2.5 py-1 border-2 bg-gray-100 border-[#4682B4] text-sm font-normal text-[#4682B4]">Full Time</button> 
+                        </div> 
+                        <div className="font-bold text-justify text-lg mt-2">UI/UX Designer</div>
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-sm font-bold mt-2">Twitter</div>
+                            <div className="text-sm font-bold mt-2">Jakarta, Indonesia</div>
+                        </div>
+                        <div className="text-sm font-light text-gray-400 my-5">Twitter is looking for UI/UX Designer to help team ma ...</div>
+                        <button className="px-2.5 py-0.5 my-3 bg-gray-100 rounded-full text-base font-medium text-green-500">Design</button> 
+                    </div>
+                    {/* Card-5 */}
+                    <div className="px-2 py-4 h-[20vw] bg-[#ffffff] hover:bg-[#4682B4] text-black hover:text-[#ffffff] transition-colors">
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-lg font-semibold uppercase">Logo</div>
+                            <button className="px-2.5 py-1 border-2 bg-gray-100 border-[#4682B4] text-sm font-normal text-[#4682B4]">Full Time</button> 
+                        </div> 
+                        <div className="font-bold text-justify text-lg mt-2">UI/UX Designer</div>
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-sm font-bold mt-2">Twitter</div>
+                            <div className="text-sm font-bold mt-2">Jakarta, Indonesia</div>
+                        </div>
+                        <div className="text-sm font-light text-gray-400 my-5">Twitter is looking for UI/UX Designer to help team ma ...</div>
+                        <button className="px-2.5 py-0.5 my-3 bg-gray-100 rounded-full text-base font-medium text-green-500">Design</button> 
+                    </div>
+                    {/* Card-6 */}
+                    <div className="px-2 py-4 h-[20vw] bg-[#ffffff] hover:bg-[#4682B4] text-black hover:text-[#ffffff] transition-colors">
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-lg font-semibold uppercase">Logo</div>
+                            <button className="px-2.5 py-1 border-2 bg-gray-100 border-[#4682B4] text-sm font-normal text-[#4682B4]">Full Time</button> 
+                        </div> 
+                        <div className="font-bold text-justify text-lg mt-2">UI/UX Designer</div>
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-sm font-bold mt-2">Twitter</div>
+                            <div className="text-sm font-bold mt-2">Jakarta, Indonesia</div>
+                        </div>
+                        <div className="text-sm font-light text-gray-400 my-5">Twitter is looking for UI/UX Designer to help team ma ...</div>
+                        <button className="px-2.5 py-0.5 my-3 bg-gray-100 rounded-full text-base font-medium text-green-500">Design</button> 
+                    </div>
+                    {/* Card-7 */}
+                    <div className="px-2 py-4 h-[20vw] bg-[#ffffff] hover:bg-[#4682B4] text-black hover:text-[#ffffff] transition-colors">
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-lg font-semibold uppercase">Logo</div>
+                            <button className="px-2.5 py-1 border-2 bg-gray-100 border-[#4682B4] text-sm font-normal text-[#4682B4]">Full Time</button> 
+                        </div> 
+                        <div className="font-bold text-justify text-lg mt-2">UI/UX Designer</div>
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-sm font-bold mt-2">Twitter</div>
+                            <div className="text-sm font-bold mt-2">Jakarta, Indonesia</div>
+                        </div>
+                        <div className="text-sm font-light text-gray-400 my-5">Twitter is looking for UI/UX Designer to help team ma ...</div>
+                        <button className="px-2.5 py-0.5 my-3 bg-gray-100 rounded-full text-base font-medium text-green-500">Design</button> 
+                    </div>
+                    {/* Card-8 */}
+                    <div className="px-2 py-4 h-[20vw] bg-[#ffffff] hover:bg-[#4682B4] text-black hover:text-[#ffffff] transition-colors">
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-lg font-semibold uppercase">Logo</div>
+                            <button className="px-2.5 py-1 border-2 bg-gray-100 border-[#4682B4] text-sm font-normal text-[#4682B4]">Full Time</button> 
+                        </div> 
+                        <div className="font-bold text-justify text-lg mt-2">UI/UX Designer</div>
+                        <div className="flex gap-2 justify-between">
+                            <div className="text-sm font-bold mt-2">Twitter</div>
+                            <div className="text-sm font-bold mt-2">Jakarta, Indonesia</div>
+                        </div>
+                        <div className="text-sm font-light text-gray-400 my-5">Twitter is looking for UI/UX Designer to help team ma ...</div>
+                        <button className="px-2.5 py-0.5 my-3 bg-gray-100 rounded-full text-base font-medium text-green-500">Design</button> 
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
