e6b37b4bbb55067d2ed8ec70a68520205cf082e6
naufalmaulana <naufallmaulana@yahoo.com>
2024-09-23 14:53:51 +0700

    Style: update auth option button width

--- jobhunter/src/app/_components/AuthOptionBtn.tsx
+++ jobhunter/src/app/_components/AuthOptionBtn.tsx
@@ -13,8 +13,8 @@ export default function AuthOptionBtn() {
   return (
     <>
         <div className="flex flex-row justify-center items-center">
-            <button className="authOptions w-24 px-2 py-2 text-steel-blue text-base font-semibold hover:bg-slate-300 bg-slate-300" onClick={(e) => authSwitch(e.currentTarget)}>Job Seeker</button>
-            <button className="authOptions w-24 px-2 py-2 text-steel-blue text-base font-semibold hover:bg-slate-300" onClick={(e) => authSwitch(e.currentTarget)}>Company</button>
+            <button className="authOptions w-1/2 px-2 py-2 text-steel-blue text-base font-semibold hover:bg-slate-300 bg-slate-300" onClick={(e) => authSwitch(e.currentTarget)}>Job Seeker</button>
+            <button className="authOptions w-1/2 px-2 py-2 text-steel-blue text-base font-semibold hover:bg-slate-300" onClick={(e) => authSwitch(e.currentTarget)}>Company</button>
         </div>
     </>
   )
