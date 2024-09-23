bd65c386475ab8cff79fcbf6963d2ffd3783f2c6
naufalmaulana <naufallmaulana@yahoo.com>
2024-09-23 23:17:21 +0700

    refractor: update hide nav and footer on dashboard logic

--- jobhunter/src/app/company/layout.tsx
+++ jobhunter/src/app/company/layout.tsx
@@ -31,7 +31,7 @@ export default async function Layout({
     <>
     <JobProvider jobs={listJob}>
       <style jsx global>{`
-          .navbar {
+          .navbarMain {
               display: none;
           }
           .footer{
