8153e54bdae27219696d8bcaee2f6420ff6ed086
naufalmaulana <naufallmaulana@yahoo.com>
2024-09-24 21:12:23 +0700

    refractor: make all svgs to jsx version

--- jobhunter/src/app/_components/EducationCard.tsx
+++ jobhunter/src/app/_components/EducationCard.tsx
@@ -18,7 +18,7 @@ export default function EducationCard() {
                 <button className="bg-transparent border-0" onClick={handleAddEducation}>
                     <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <rect x="0.5" y="0.5" width="39" height="39" stroke="#D6DDEB"/>
-                        <g clip-path="url(#clip0_178_5701)">
+                        <g clipPath="url(#clip0_178_5701)">
                         <path fillRule="evenodd" clipRule="evenodd" d="M20 12C20.5523 12 21 12.4477 21 13V27C21 27.5523 20.5523 28 20 28C19.4477 28 19 27.5523 19 27V13C19 12.4477 19.4477 12 20 12Z" fill="#4D7EA8"/>
                         <path fillRule="evenodd" clipRule="evenodd" d="M12 20C12 19.4477 12.4477 19 13 19H27C27.5523 19 28 19.4477 28 20C28 20.5523 27.5523 21 27 21H13C12.4477 21 12 20.5523 12 20Z" fill="#4D7EA8"/>
                         </g>
--- jobhunter/src/app/_components/SearchBar.tsx
+++ jobhunter/src/app/_components/SearchBar.tsx
@@ -13,8 +13,8 @@ export default function SearchBar(props: Props) {
             <div className="flex flex-wrap items-center justify-center lg:justify-between gap-2 lg:gap-2">
               <label className="input input-bordered flex items-center gap-2 w-full lg:w-auto grow">
                 <svg className="h-4 w-4 opacity-70" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="11.7666" cy="11.7666" r="8.98856" stroke="#25324B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                  <path d="M18.0183 18.4851L21.5423 22" stroke="#25324B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  <circle cx="11.7666" cy="11.7666" r="8.98856" stroke="#25324B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                  <path d="M18.0183 18.4851L21.5423 22" stroke="#25324B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
                 <input 
                   type="text" 
