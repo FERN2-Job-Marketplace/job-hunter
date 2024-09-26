940e96eaec688519a1f679a014a1f79a3e75d0f9
dianafitriaw <dianafit72@gmail.com>
2024-09-26 18:24:22 +0700

    my profile

--- jobhunter/src/app/_components/ExperienceCardForm.tsx
+++ jobhunter/src/app/_components/ExperienceCardForm.tsx
@@ -1,35 +1,80 @@
-export default function ExperienceCardForm() {
+import { ChangeEvent } from "react"
+import { myProfileForm } from "../user/myprofile/hooks"
+
+type Props = {
+    input: myProfileForm
+    handleChangeInput: (event: ChangeEvent<HTMLSelectElement | HTMLInputElement | HTMLTextAreaElement>) => void
+}
+
+export default function ExperienceCardForm(props: Props) {
   return (
     <>
+        <div className="formCard border border-solid border-slate-300 rounded w-full max-w-lg p-3 my-5">
         <div className="formCardBody py-[24px]">
             <p className="mb-3 text-raisin-black font-semibold capitalize text-[18px]">
-                Job Experience
+                Last Job Experience
             </p>
-            <input type="text" placeholder="Job Title" className="input input-bordered w-full mb-3" />
+            <input
+            value={props.input.jobtitle}
+            onChange={props.handleChangeInput}
+             name="jobtitle"
+             type="text" 
+             placeholder="Job Title" 
+             className="input input-bordered w-full mb-3" />
             <div className="flex items-center justify-between w-full mb-3">
-                <input type="text" placeholder="Company Name" className="input input-bordered w-5/12" />
-                <select className="select select-bordered text-raisin-black w-5/12">
-                    <option disabled selected>Job Type</option>
+                <input 
+                value={props.input.company}
+                onChange={props.handleChangeInput}
+                name="company"
+                type="text" 
+                placeholder="Company Name" 
+                className="input input-bordered w-5/12" />
+                <select
+                    name="jobtype"
+                    value={props.input.jobtype}
+                    onChange={props.handleChangeInput}
+                    defaultValue={""}
+                    className="select select-bordered text-white w-5/12"
+                >
+                    <option value={''}>Job Type</option>
                     <option>Full Time</option>
                     <option>Contract</option>
                     <option>Part Time</option>
                     <option>Internship</option>
                 </select>
             </div>
-            <input type="Number" placeholder="Duration (Years)" className="input input-bordered w-full mb-3" />
-            <select className="select select-bordered text-raisin-black w-full mb-3">
-                <option disabled selected>Location</option>
-                <option>Jakarta Pusat</option>
-                <option>Jakarta Selatan</option>
-                <option>Jakarta Utara</option>
-                <option>Jakarta Barat</option>
-                <option>Jakarta Timur</option>
-                <option>Bogor</option>
-                <option>Depok</option>
-                <option>Tangerang</option>
-                <option>Bekasi</option>
+            <input 
+            value={props.input.durationexperience}
+            onChange={props.handleChangeInput}
+            name="durationexperience"
+            type="Number" 
+            placeholder="Duration (Years)" 
+            className="input input-bordered w-full mb-3" />
+            <select
+                name="location"
+                value={props.input.location}
+                onChange={props.handleChangeInput} 
+                defaultValue={""}
+                className="select select-bordered text-white w-full mb-3"
+            >
+                <option value={''}>Location</option>
+                <option >Jakarta Pusat</option>
+                <option >Jakarta Selatan</option>
+                <option >Jakarta Utara</option>
+                <option >Jakarta Barat</option>
+                <option >Jakarta Timur</option>
+                <option >Bogor</option>
+                <option >Depok</option>
+                <option >Tangerang</option>
+                <option >Bekasi</option>
             </select>
-            <textarea className="textarea textarea-bordered h-36 w-full" placeholder="Job Description"></textarea>
+            <textarea 
+            name="jobdescription"
+            onChange={props.handleChangeInput} 
+            value={props.input.jobdescription}
+            className="textarea textarea-bordered h-36 w-full" 
+            placeholder="Job Description"></textarea>
+        </div>
         </div>
     </>
   )
--- jobhunter/src/app/user/myprofile/page.tsx
+++ jobhunter/src/app/user/myprofile/page.tsx
@@ -1,7 +1,12 @@
-import EducationCard from "@/app/_components/EducationCard";
-import ExperienceCard from "@/app/_components/ExperienceCard";
+'use client'
+
+import EducationCardForm from "@/app/_components/EducationCardForm";
+import ExperienceCardForm from "@/app/_components/ExperienceCardForm";
+import { useMyProfile } from "./hooks";
 
 export default function MyProfile() {
+    const { input, handleChangeImage, handleChangeInput, handleSaveProfile } = useMyProfile()
+
   return (
     <>
         <div className="dashboardProfile py-4 px-5">
@@ -10,66 +15,119 @@ export default function MyProfile() {
                     <div className="label">
                         <span className="label-text">Select Profile Image</span>
                     </div>
-                    <input type="file" accept="image/*" className="file-input file-input-bordered w-full" />
+                    <input 
+                    onChange={handleChangeImage}
+                    name="image"
+                    type="file" 
+                    accept="image/*" 
+                    className="file-input file-input-bordered w-full" />
                 </label>
                 <label className="form-control w-full max-w-sm mb-3">
                     <div className="label">
                         <span className="label-text capitalize text-slate-grey">full name</span>
                     </div>
-                    <input type="text" placeholder="Full Name" className="input input-bordered w-full" />
+                    <input 
+                    value={input.name}
+                    onChange={handleChangeInput}
+                    type="text" 
+                    placeholder="Full Name"
+                    name="name"
+                    className="input input-bordered w-full" />
                 </label>
                 <label className="form-control w-full max-w-sm mb-3">
                     <div className="label">
                         <span className="label-text capitalize text-slate-grey">email</span>
                     </div>
-                    <input type="text" placeholder="Email" className="input input-bordered w-full" />
+                    <input 
+                    value={input.email}
+                    onChange={handleChangeInput}
+                    type="text" 
+                    name="email"
+                    placeholder="Email" 
+                    className="input input-bordered w-full" />
                 </label>
                 <label className="form-control w-full max-w-sm mb-3">
                     <div className="label">
                         <span className="label-text capitalize text-slate-grey">phone number</span>
                     </div>
-                    <input type="number" placeholder="081234567890" className="input input-bordered w-full" />
+                    <input 
+                    value={input.number}
+                    onChange={handleChangeInput}
+                    type="number" 
+                    name="number"
+                    placeholder="081234567890" 
+                    className="input input-bordered w-full" />
                 </label>
                 <label className="form-control w-full max-w-sm mb-3">
                     <div className="label">
                         <span className="label-text capitalize text-slate-grey">birth date</span>
                     </div>
-                    <input type="date" placeholder="Birth Date" className="input input-bordered w-full" />
+                    <input 
+                    value={input.birthdate}
+                    onChange={handleChangeInput}
+                    type="date" 
+                    name="birthdate"
+                    placeholder="Birth Date" 
+                    className="input input-bordered w-full" />
                 </label>
                 <label className="form-control w-full max-w-sm mb-3">
                     <div className="label">
                         <span className="label-text capitalize text-slate-grey">gender</span>
                     </div>
-                    <select className="select select-bordered text-raisin-black w-full">
-                        <option disabled selected>Select Gender</option>
-                        <option>Male</option>
-                        <option>Female</option>
-                        <option>Apache Helicopter</option>
+                    <select 
+                        value={input.gender}
+                        onChange={handleChangeInput}
+                        name="gender"
+                        defaultValue={""}
+                        className="select select-bordered text-raisin-black w-full"
+                    >
+                        <option value="">Select Gender</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
                     </select>
                 </label>
                 <label className="form-control w-full max-w-sm mb-3">
                     <div className="label">
                         <span className="label-text capitalize text-slate-grey">Years of Experience</span>
                     </div>
-                    <input type="number" placeholder="Years of Experience" className="input input-bordered w-full" />
+                    <input 
+                    value={input.yearsexperience}
+                    onChange={handleChangeInput}
+                    name="yearsexperience"
+                    type="number" 
+                    placeholder="Years of Experience" 
+                    className="input input-bordered w-full" />
                 </label>
                 <label className="form-control w-full max-w-sm mb-3">
                     <div className="label">
                         <span className="label-text capitalize text-slate-grey">about me</span>
                     </div>
-                    <textarea className="textarea textarea-bordered h-24" placeholder="About Me"></textarea>
+                    <textarea 
+                    value={input.about}
+                    onChange={handleChangeInput}
+                    name="about"
+                    className="textarea textarea-bordered h-24" 
+                    placeholder="About Me"></textarea>
                 </label>    
-                <ExperienceCard/>
-                <EducationCard/>
+                <ExperienceCardForm handleChangeInput={handleChangeInput} input={input} />
+                <EducationCardForm handleChangeInput={handleChangeInput} input={input} />
                 <div className="formCard border border-solid border-slate-300 rounded w-full max-w-lg p-3 my-5">
                     <div className="formCardHeader flex items-center justify-between mb-3">
                         <p className="m-0 text-raisin-black font-semibold capitalize text-[16px]">
                             skills
                         </p>
                     </div>
-                    <textarea className="textarea textarea-bordered h-36 w-full" placeholder="List Your Skills"></textarea>
+                    <textarea 
+                    value={input.skills}
+                    onChange={handleChangeInput}
+                    name="skills"
+                    className="textarea textarea-bordered h-36 w-full" 
+                    placeholder="List Your Skills"></textarea>
                 </div>
-                <button className="bg-steel-blue block text-white font-bold capitalize border border-solid border-steel-blue text-center transition px-[24px] py-[12px] hover:text-steel-blue hover:bg-white">
+                <button 
+                    type="button"
+                    onClick={handleSaveProfile}
+                    className="bg-steel-blue block text-white font-bold capitalize border border-solid border-steel-blue text-center transition px-[24px] py-[12px] hover:text-steel-blue hover:bg-white">
                     Save Profile
                 </button>
             </form>
